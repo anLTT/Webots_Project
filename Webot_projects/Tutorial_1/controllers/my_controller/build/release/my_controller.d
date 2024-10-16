b6c4eb2a57bdab56c22bc68af784e13ea35d1a58
anLTT <2396029174@qq.com>
2024-10-16 22:31:16 +0800

    Learn tutorial_1

---
+++ Webot_projects/Tutorial_1/controllers/my_controller/build/release/my_controller.d
@@ -0,0 +1,5 @@
+build/release/my_controller.o: my_controller.c \
+ E:\software\webots-R2021a/include/controller/c/webots/robot.h \
+ E:\software\webots-R2021a/include/controller/c/webots/types.h \
+ E:\software\webots-R2021a/include/controller/c/webots/nodes.h \
+ E:\software\webots-R2021a/include/controller/c/webots/motor.h
